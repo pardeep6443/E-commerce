80858dc0bb20817723d9e45c79829fa7282751af
Pardeep Singla <116626777+pardeep6443@users.noreply.github.com>
2025-08-31 13:15:31 +0530

    Fix theme provider hydration error

--- components/providers.tsx
+++ components/providers.tsx
@@ -9,7 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="system"
       enableSystem
       disableTransitionOnChange={false}
     >
